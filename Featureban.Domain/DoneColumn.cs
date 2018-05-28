00e4773c6f6368f9e767be008ce710c0d7b08d02
Dmitrii Ivanov <d.ivanov@dodopizza.com>
2018-05-28 19:03:48 +0300

    Done column introduced.

---
+++ Featureban.Domain/DoneColumn.cs
@@ -0,0 +1,12 @@
+﻿namespace Featureban.Domain
+{
+	internal class DoneColumn
+	{
+		public int CardCount { get; private set; }
+
+		public void AddCard(Card card)
+		{
+			CardCount++;
+		}
+	}
+}
---
+++ Featureban.Tests/DoneColumnTest.cs
@@ -0,0 +1,20 @@
+﻿using Featureban.Domain;
+using Featureban.Tests.DSL;
+using Xunit;
+
+namespace Featureban.Tests
+{
+	public class DoneColumnTest
+	{
+		[Fact]
+		public void AddCard_AddsCard()
+		{
+			var column = Create.DoneColumn
+				.Please();
+
+			column.AddCard(Create.Card.Please());
+			
+			Assert.Equal(1, column.CardCount);
+		}
+	}
+}
