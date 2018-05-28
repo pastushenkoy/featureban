ed5034fea15fe331b99a3c02f02be122e5dbb4a5
Dmitrii Ivanov <d.ivanov@dodopizza.com>
2018-05-28 18:53:01 +0300

    Todo column introduced.

---
+++ Featureban.Tests/TodoColumnTests.cs
@@ -0,0 +1,22 @@
+﻿using Featureban.Tests.DSL;
+using Xunit;
+
+namespace Featureban.Tests
+{
+	
+	public class TodoColumnTests
+	{
+		[Fact]
+		public void ExtractCard_Success()
+		{
+			var player = 1;
+			var column = Create
+				.ToDoColumn
+				.Please();
+
+			var card = column.ExtractCardFor(player);
+			
+			Assert.NotNull(card);
+		}
+	}
+}
